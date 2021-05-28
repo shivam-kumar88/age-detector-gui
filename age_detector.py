251560ddcda7ef7385ec4d15567b09c1dd30c7af
shivam <shivam882531@gmail.com>
2021-05-28 13:42:08 +0530

    secound commit

--- age_detector.py
+++ age_detector.py
@@ -6,23 +6,9 @@ root = Tk()
 root.geometry("700x1000")
 root.title("age calculator")
 
-photo = PhotoImage(file="picture_shivam.png")
+photo = PhotoImage(file="picture_shivam.png")  #put your image with .png extensions
 myimage = Label(image=photo)
 myimage.grid(row=0, column=1)
-
-def agecalculate():
-    today = date.today()
-    birthDate = date(int(yearEntry.get()), int(monthEntry.get()), int(dayEntry.get()))
-
-    age = today.year - birthDate.year - ((today.month, today.day)<(birthDate.month, birthDate.day))
-    Label(text = f"{nameValue.get()} your age is {age}").grid(row=6,column=1)
-
-
-
-Label(text="name").grid(row=1, column=0, padx=90)
-Label(text="year").grid(row=2, column=0)
-Label(text="month").grid(row=3, column=0)
-Label(text="day").grid(row=4, column=0)
 
 nameValue = StringVar()
 yearValue = StringVar()
@@ -38,6 +24,22 @@ nameEntry.grid(row=1, column=1, padx=10)
 yearEntry.grid(row=2, column=1, padx=10)
 monthEntry.grid(row=3, column=1, padx=10)
 dayEntry.grid(row=4, column=1, padx=10)
+
+def agecalculate():
+    today = date.today()
+    birthDate = date(int(yearEntry.get()), int(monthEntry.get()), int(dayEntry.get()))
+
+    age = today.year - birthDate.year - ((today.month, today.day)<(birthDate.month, birthDate.day))
+    Label(text = f"{nameValue.get()} your age is {age}").grid(row=6,column=1)
+
+
+
+Label(text="name").grid(row=1, column=0, padx=90)
+Label(text="year").grid(row=2, column=0)
+Label(text="month").grid(row=3, column=0)
+Label(text="day").grid(row=4, column=0)
+
+
 
 Button(text="find age", command=agecalculate).grid(row=5, column=1, padx=10)
 
